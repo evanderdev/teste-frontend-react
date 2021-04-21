65e9fbf8ac134dfafa5c4a34a1ebe697e26d4b83
Evander Oliveira <e_vander_oliveira@hotmail.com>
2021-04-21 19:27:03 -0300

    feita prateleira, falta estilo

--- src/components/Shelf/Shelf.tsx
+++ src/components/Shelf/Shelf.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Slider from "react-slick";
 
-import { useProduct } from "../../contexts/ProductsContext";
-
 import ProductCard from "../ProductCard/ProductCard";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./styles.scss";
+
+import { useProduct } from "../../contexts/ProductsContext";
 
 const Shelf: React.FC = () => {
 	const { products } = useProduct();
@@ -16,16 +16,15 @@ const Shelf: React.FC = () => {
 		arrows: false,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
+		slidesToShow: 4,
+		slidesToScroll: 4,
 	};
 
 	return (
 		<Slider {...settings}>
-			{products &&
-				products.map((product: IProduct) => {
-					<ProductCard product={product} />;
-				})}
+			{products?.map((product: IProduct) => {
+				return <ProductCard key={product.productId} product={product} />;
+			})}
 		</Slider>
 	);
 };
